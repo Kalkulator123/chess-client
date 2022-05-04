f688279ec4bac04ffb3473e5bfa5f71c0b56b05f
NBright04 <nbright.pl@gmail.com>
2022-05-04 14:53:42 +0200

    added useState support

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Square from './Square';
 import {BetterFen} from './BetterFen';
+import { PostionChecker } from './PositionChecker';
 
 function Board(props:any) {
+    const [selectedSquare , setSelectedSquare] = useState(-1);
+    let positions = [{x: -1, y:-1}];
     let arr = [];
+
+    const updateSelectedSquare = (selectedItem: any) => {
+        setSelectedSquare(selectedItem);
+        generateLegalMoves(selectedItem);
+
+        for(let i = 0;i<8;i++){
+            for(let j = 0;j<8;j++){
+                a++;
+                //ReactDOM.render(<Square id={i}/>, document.getElementById("board"));
+                const piece = BetterFen.getPiece(i,j);
+                const team = BetterFen.getTeam(i,j);
+
+                positions.forEach(position => {
+                    if(j == position.x
+                    && i == position.y) {
+                        arr.push(<Square id={a} isGood={true} isBlack={a%2===0} team={team} piece={piece} parentCallback={updateSelectedSquare}/>);
+                    } else {
+                        arr.push(<Square id={a} isBlack={a%2===0} team={team} piece={piece} parentCallback={updateSelectedSquare}/>);
+                    }
+                });
+            }
+        }
+    };
+
+    const generateLegalMoves = (id: number) => {
+        let positionChecker:PostionChecker = new PostionChecker(BetterFen.value, false);
+        id--;
+        const unit = BetterFen.value[Math.floor(id/8)][id%8];
+        if(unit.team===(positionChecker.isWhite ? 1 : 0) && selectedSquare===-1){
+            setSelectedSquare(id);
+            positions = positionChecker.checkMoves(Math.floor(id/8), id%8);
+            console.log(positions);
+        }else if(selectedSquare!==-1){
+            //TODO sprawdzanie ruchu  
+        }
+    };
+
     let a = 0;
     for(let i = 0;i<8;i++){
         for(let j = 0;j<8;j++){
@@ -11,7 +51,7 @@ function Board(props:any) {
             //ReactDOM.render(<Square id={i}/>, document.getElementById("board"));
             const piece = BetterFen.getPiece(i,j);
             const team = BetterFen.getTeam(i,j);
-            arr.push(<Square id={a} isBlack={a%2===0} team={team} piece={piece}/>);
+            arr.push(<Square id={a} isBlack={a%2===0} team={team} piece={piece} parentCallback={updateSelectedSquare}/>);
         }
     }
 
--- src/Square.tsx
+++ src/Square.tsx
@@ -4,20 +4,6 @@ import { BetterFen } from "./BetterFen";
 import { PostionChecker } from './PositionChecker';
 let selectedSquare:number = -1;
 
-function onClick(id:number){
-    let positionChecker:PostionChecker = new PostionChecker(BetterFen.value, false);
-    id--;
-    const unit = BetterFen.value[Math.floor(id/8)][id%8];
-    if(unit.team===(positionChecker.isWhite ? 1 : 0) && selectedSquare===-1){
-        selectedSquare = id;
-        console.log(positionChecker.checkMoves(Math.floor(id/8), id%8));
-        
-    }else if(selectedSquare!==-1){
-        //TODO sprawdzanie ruchu
-        
-    }
-}
-
 function Square(props:any) {
     const color = (id:number, isBlack:boolean) => {
         const row:number = Math.floor((id-1)/8);
@@ -28,16 +14,16 @@ function Square(props:any) {
             if(!isBlack) return "black";
             else return "white";
         } 
-                
-        
     }
 
-
     //console.log(props.team);
-    const classes:string = color(props.id, props.isBlack)+" "+props.team+" "+props.piece;
+    let classes:string = color(props.id, props.isBlack)+" "+props.team+" "+props.piece;
+    if(props.isGood) {
+        classes += " good";
+    }
     
     return <input type={"button"} id={props.id} className={classes} onClick={() => {
-        onClick(props.id);
+        props.parentCallback(props.id)
     }}></input>;
 }
 
