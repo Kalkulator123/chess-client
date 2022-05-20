de18851a721fabe09ddc3ebe3f8e54209cfa67b1
pabelito04 <pabelito04@gmail.com>
2022-05-20 12:54:27 +0200

    zabezpieczono ruchy

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -13,7 +13,8 @@ import {
   Position,
   samePosition,
 } from "../../Constants";
-
+let canMove:boolean = true;
+let isWhite = true;
 export default function Chessboard() {
   const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
   const [promotionPawn, setPromotionPawn] = useState<Piece>();
@@ -22,67 +23,77 @@ export default function Chessboard() {
   const chessboardRef = useRef<HTMLDivElement>(null);
   const modalRef = useRef<HTMLDivElement>(null);
   const referee = new Referee();
+  
 
   function grabPiece(e: React.MouseEvent) {
-    const element = e.target as HTMLElement;
-    const chessboard = chessboardRef.current;
-    if (element.classList.contains("chess-piece") && chessboard) {
-      const grabX = Math.floor((e.clientX - chessboard.offsetLeft) / GRID_SIZE);
-      const grabY = Math.abs(
-        Math.ceil((e.clientY - chessboard.offsetTop - 800) / GRID_SIZE)
-      );
-      setGrabPosition({ x: grabX, y: grabY });
-
-      const x = e.clientX - GRID_SIZE / 2;
-      const y = e.clientY - GRID_SIZE / 2;
-      element.style.position = "absolute";
-      element.style.left = `${x}px`;
-      element.style.top = `${y}px`;
-
-      setActivePiece(element);
+    if(canMove){
+      const element = e.target as HTMLElement;
+      const chessboard = chessboardRef.current;
+      if (element.classList.contains("chess-piece") && chessboard) {
+        const grabX = Math.floor((e.clientX - chessboard.offsetLeft) / GRID_SIZE);
+        const grabY = Math.abs(
+          Math.ceil((e.clientY - chessboard.offsetTop - 800) / GRID_SIZE)
+        );
+        const currentPiece = pieces.find((p) =>
+          samePosition(p.position, { x: grabX, y: grabY })
+        );
+        if((isWhite && currentPiece?.team==TeamType.OUR)||(!isWhite && currentPiece?.team==TeamType.OPPONENT)){
+          setGrabPosition({ x: grabX, y: grabY });
+  
+          const x = e.clientX - GRID_SIZE / 2;
+          const y = e.clientY - GRID_SIZE / 2;
+          element.style.position = "absolute";  
+          element.style.left = `${x}px`;
+          element.style.top = `${y}px`;
+          setActivePiece(element);
+        }
+        
+    }
+    
     }
   }
 
   function movePiece(e: React.MouseEvent) {
-    const chessboard = chessboardRef.current;
-    if (activePiece && chessboard) {
-      const minX = chessboard.offsetLeft - 25;
-      const minY = chessboard.offsetTop - 25;
-      const maxX = chessboard.offsetLeft + chessboard.clientWidth - 75;
-      const maxY = chessboard.offsetTop + chessboard.clientHeight - 75;
-      const x = e.clientX - 50;
-      const y = e.clientY - 50;
-      activePiece.style.position = "absolute";
+      const chessboard = chessboardRef.current;
+      if (activePiece && chessboard) {
+        const minX = chessboard.offsetLeft - 25;
+        const minY = chessboard.offsetTop - 25;
+        const maxX = chessboard.offsetLeft + chessboard.clientWidth - 75;
+        const maxY = chessboard.offsetTop + chessboard.clientHeight - 75;
+        const x = e.clientX - 50;
+        const y = e.clientY - 50;
+        activePiece.style.position = "absolute";
 
-      //If x is smaller than minimum amount
-      if (x < minX) {
-        activePiece.style.left = `${minX}px`;
-      }
-      //If x is bigger than maximum amount
-      else if (x > maxX) {
-        activePiece.style.left = `${maxX}px`;
-      }
-      //If x is in the constraints
-      else {
-        activePiece.style.left = `${x}px`;
-      }
+        //If x is smaller than minimum amount
+        if (x < minX) {
+          activePiece.style.left = `${minX}px`;
+        }
+        //If x is bigger than maximum amount
+        else if (x > maxX) {
+          activePiece.style.left = `${maxX}px`;
+        }
+        //If x is in the constraints
+        else {
+          activePiece.style.left = `${x}px`;
+        }
 
-      //If y is smaller than minimum amount
-      if (y < minY) {
-        activePiece.style.top = `${minY}px`;
+        //If y is smaller than minimum amount
+        if (y < minY) {
+          activePiece.style.top = `${minY}px`;
+        }
+        //If y is bigger than maximum amount
+        else if (y > maxY) {
+          activePiece.style.top = `${maxY}px`;
+        }
+        //If y is in the constraints
+        else {
+          activePiece.style.top = `${y}px`;
+        }
       }
-      //If y is bigger than maximum amount
-      else if (y > maxY) {
-        activePiece.style.top = `${maxY}px`;
-      }
-      //If y is in the constraints
-      else {
-        activePiece.style.top = `${y}px`;
-      }
-    }
   }
 
   function dropPiece(e: React.MouseEvent) {
+    if(canMove){
     const chessboard = chessboardRef.current;
     if (activePiece && chessboard) {
       const x = Math.floor((e.clientX - chessboard.offsetLeft) / GRID_SIZE);
@@ -114,6 +125,7 @@ export default function Chessboard() {
         const pawnDirection = currentPiece.team === TeamType.OUR ? 1 : -1;
 
         if (isEnPassantMove) {
+          canMove = false;
           const updatedPieces = pieces.reduce((results, piece) => {
             if (samePosition(piece.position, grabPosition)) {
               piece.enPassant = false;
@@ -136,6 +148,7 @@ export default function Chessboard() {
         } else if (validMove) {
           //UPDATES THE PIECE POSITION
           //AND IF A PIECE IS ATTACKED, REMOVES IT
+          canMove = false;
           const updatedPieces = pieces.reduce((results, piece) => {
             if (samePosition(piece.position, grabPosition)) {
               //SPECIAL MOVE
@@ -173,6 +186,7 @@ export default function Chessboard() {
       }
       setActivePiece(null);
     }
+  }
   }
 
   function promotePawn(pieceType: PieceType) {
